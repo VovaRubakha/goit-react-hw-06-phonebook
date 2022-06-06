b5181382b6d22c4b089a11f72222129b914dabf8
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-06-06 22:39:07 +0300

    some fixes

--- src/components/redux/contacts/contactsSelectors.js
+++ src/components/redux/contacts/contactsSelectors.js
@@ -1,6 +1,1 @@
 export const getContacts = state => state.contacts.items;
-
-export const deleteContact = state =>
-  state.contacts.items.filter(contact => contact.id);
-
-// export const setFilter = state => state.contacts;
